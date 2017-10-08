f3c9de4981f9dd8752147d55013ee92545137325
miki <miodragmilosevic84miki@gmail.com>
2017-10-08 10:41:37 +0200

    check uncheck

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -5,6 +5,7 @@ var TodoSearch = require('TodoSearch');
 var uuid = require('node-uuid');
 
 
+
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
@@ -13,16 +14,20 @@ var TodoApp = React.createClass({
       todos: [
         {
         id: uuid(),
-        text: 'Walk the dog'
+        text: 'Walk the dog',
+        completed:false
       }, {
         id: uuid(),
-        text: 'Clean the yard'
+        text: 'Clean the yard',
+        completed: true
       }, {
         id: uuid(),
-        text: 'tri'
+        text: 'tri',
+        completed: true
       }, {
         id: uuid(),
-        text: 'cetiri'
+        text: 'cetiri',
+        completed: false
       }
       ]
     };
@@ -33,10 +38,20 @@ this.setState({
     ...this.state.todos,
     {
       id: uuid(),
-      text: text
+      text: text,
+      completed: false
     }
   ]
 });
+  },
+  handleToggle: function (id) {
+    var updateTodos = this.state.todos.map((todo) => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todos;
+    });
+    this.setState({todos: updateTodos});
   },
   handleSearch: function (showCompleted, searchText) {
     this.setState({
@@ -49,7 +64,7 @@ this.setState({
   return (
     <div>
       <TodoSearch onSearch={this.handleSearch}/>
-<TodoList todos={todos}/>
+<TodoList todos={todos} onToggle={this.handleToggle}/>
 <AddTodo onAddTodo={this.handleAddTodo}/>
     </div>
   )
